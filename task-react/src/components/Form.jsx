b61137baef62e68673ec30d5540d517dbbec1fab
Piotr-reader <piotr_partsianko@inbox.ru>
2021-04-20 14:36:26 +0300

    feat: implement function open close popup

--- task-react/src/components/Form.jsx
+++ task-react/src/components/Form.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import s from './Form.module.css';
 
 const Form = () => {
+
+  let hendleSubmit = (event) => {
+    event.preventDefault();
+    document.querySelector('#popup').style.display = 'block';
+  }
   return (
+
     <div className={s.wrapper}>
 
-        <form className={s.form} action='#' >
+        <form className={s.form} action='#' onSubmit={hendleSubmit}>
 
-            <label for='name' className={s.label}>Ваше имя:</label>
+            <label htmlFor='name' className={s.label}>Ваше имя:</label>
             <p><input id='name' className={s.input} type='text' name='name' placeholder='Name' required/></p>
 
-            <label for='surname' className={s.label}>Ваша фамилия:</label>
+            <label htmlFor='surname' className={s.label}>Ваша фамилия:</label>
             <p><input id='surname' className={s.input} type='text' name='surname' placeholder='Surname' required/></p>
 
             <p><input id='submit' type='submit' value='Готово' className={s.submit}/></p>
--- task-react/src/components/PopUp.jsx
+++ task-react/src/components/PopUp.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import s from './PopUp.module.css';
 
 const PopUp = () => {
+
+  let closePopUp = () => {
+    document.querySelector('#popup').style.display = 'none';
+  }
+
   return (
     <div id='popup' className={s.popup}>
         <span className={s.text}>Здравствуйте, </span>
-        <p><input id='close' type='submit' value='Закрыть' className={s.close}/></p>
+        <p><input id='close' type='submit' value='Закрыть' className={s.close} onClick={closePopUp}/></p>
     </div>
   );
 }
